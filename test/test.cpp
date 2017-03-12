2bfcf6586f76ed6a53c5ef4e9b9ee3daf6d22cf5
zzjkf2009 <zzeng@terpmail.umd.edu>
2017-03-12 19:55:07 -0400

    add google unit test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,47 @@
 #include <gtest/gtest.h>
+#include"Astar.h"
+#include"../app/Astar.cpp"
 
-TEST(dummy, should_pass)
-{
-    EXPECT_EQ(1, 1);
+typedef std::pair<int, int> coordinate;
+
+Astar Testastar;
+
+TEST(Astartest, map_contrain){
+
+
+EXPECT_EQ(Testastar.isValid(40,90),0);
+
+}
+
+
+
+TEST(Astartest, testGoal){
+
+  coordinate goal=std::make_pair(8,9);
+  EXPECT_EQ(Testastar.isGoalNode(8,9,goal),1);
+}
+
+TEST(Astartest, AstarAlgorithm){
+
+  int grid[10][10]=
+  {
+     {1,0,1,1,1,1,0,1,1,1},
+     {1,1,1,0,1,1,1,0,1,1},
+     {1,1,1,0,1,1,0,1,0,1},
+     {0,0,1,0,1,0,0,0,0,1},
+     {1,1,1,0,0,1,1,0,1,0},
+     {1,0,1,1,0,1,1,0,1,0},
+     {1,0,1,0,0,1,0,1,1,0},
+     {1,1,1,0,0,1,0,0,1,1},
+     {0,0,1,1,0,1,1,1,0,1},
+     {0,1,0,0,1,1,0,0,1,0}
+  };
+
+  coordinate start=std::make_pair(0,0);
+  coordinate goal=std::make_pair(8,9);
+  std::stack < coordinate > TestPath;
+  int weight=5;
+  TestPath=Testastar.WeightedAstar(grid,start,goal,weight);
+  EXPECT_NE(TestPath.empty(),1);
+
 }
